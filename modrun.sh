7a1f597d214050cbc13a6875968dea99d925efe6
Alvaro Valdebenito <alvarov@met.no>
2017-09-04 16:16:58 +0200

    2015 EECCA config and job

--- modrun.sh
+++ modrun.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Minimalistic script for run the Unified EMEP model
+GRID=EECCA
+NLEV=20lev
+cd ~/work/EMEP_MSC-W_model.rv4.15.OpenSource/Base_${GRID}_${NLEV}
 
 # Link the input data
 inputdir=../
-ln -s $inputdir/input/* .   # input files except meteorology
+ln -s $inputdir/input/* .        # input files except meteorology
+ln -s $inputdir/input/$NLEV/* .  # num-level dependant input
+ln -s $inputdir/input/$GRID/* .  # grid dependant input
 
 # Run the model
 mpiexec $inputdir/code/Unimod
 
 # Clean the links to the input data
-find . -type l -print0 | xargs -0 rm 
+find -maxdepth 1 -type l -delete
