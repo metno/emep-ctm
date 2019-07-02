eb1fc78b1cddae33e49aba3c83bc03b2f8326be5
Alvaro Valdebenito <alvarov@met.no>
2019-07-02 11:24:21 +0200

    update version

--- modrun.sh
+++ modrun.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Minimalistic script for run the Unified EMEP model
-GRID=EECCA
-NLEV=20lev
-cd ~/work/EMEP_MSC-W_model.rv4.17.OpenSource/Base_${GRID}_${NLEV}
+### job options for Slurm/sbatch
+#SBATCH --job-name=emepctm
+#SBATCH --output=%x.out --error=%x.out
+#SBATCH --nodes=2 --ntasks-per-node=32 --time=5:00:00
 
-# Run the model
-mpiexec ../code/Unimod
+### Minimalistic script for run the Unified EMEP model
+
+# working directory
+cd ~/work/EMEP_MSC-W_model.rv4.33.OpenSource/Base
+
+# run the model
+#mpiexec ../code/emepctm # or
+#mpirun  ../code/emepctm # or
+mpprun   ../code/emepctm # depending on the HPC/queue
+
+
+
