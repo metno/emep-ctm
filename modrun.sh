c49928ad414354271d9b5fd7e6b6a44c1cbf49ac
Alvaro Valdebenito <alvarov@met.no>
2026-09-17 13:47:14 +0200

    update benchmark config

--- modrun.sh
+++ modrun.sh
@@ -3,12 +3,12 @@
 ### job options for Slurm/sbatch
 #SBATCH --job-name=emepctm
 #SBATCH --output=%x.out --error=%x.out
-#SBATCH --nodes=4 --ntasks-per-node=32 --time=18:00:00
+#SBATCH --nodes=1 --ntasks-per-node=144 --time=18:00:00
 
 ### Minimalistic script for run the Unified EMEP model
 
 # working directory
-cd ~/work/EMEP_MSC-W_model.v5.6.OpenSource/Base
+cd ~/work/EMEP_MSC-W_model.v5.8.OpenSource/Base
 
 # run the model
 #mpiexec ../code/emepctm # or
